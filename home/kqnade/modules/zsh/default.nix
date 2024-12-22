bacd670d6cd04a35cb42484a47bb3a2dfa6d44bb
kqnade <git@k4na.de>
2024-12-22 20:23:59 +0900

    :fire: Remove: Clear zsh plugins

--- home/kqnade/modules/zsh/default.nix
+++ home/kqnade/modules/zsh/default.nix
@@ -11,20 +11,9 @@
       vi = "nvim";
       vim = "nvim";
     };
-    enableCompletion = true;
-    autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
-    zplug = {
-      enable = true;
-      plugins = [
-        { name = "zsh-users/zsh-autosuggestions"; }
-        { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; }
-      ];
+
     };
-    initExtra = ''
-      [[ ! -f ${./zsh/p10k.zsh} ]] || source ${./zsh/p10k.zsh}
-    '';
-    
   };
 
 }
