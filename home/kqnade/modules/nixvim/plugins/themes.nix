7099ab0c9bca1167c896bcc1dd9d2e902f139c29
kqnade <git@k4na.de>
2025-04-30 19:06:25 +0900

    :art: Update: chnage nixvim theme to melange

--- home/kqnade/modules/nixvim/plugins/themes.nix
+++ home/kqnade/modules/nixvim/plugins/themes.nix
@@ -1,25 +1,5 @@
 {
-  colorschemes.catppuccin = {
+  colorschemes.melange = {
     enable = true;
-
-    settings = {
-      disable_underline = true;
-      flavour = "frappe";
-      integrations = {
-        cmp = true;
-        gitsigns = true;
-        notify = false;
-        nvimtree = true;
-        treesitter = true;
-      };
-      styles = {
-        booleans = [
-          "bold"
-          "italic"
-        ];
-        conditionals = [ "bold" ];
-      };
-      term_colors = true;
-    };
   };
 }
