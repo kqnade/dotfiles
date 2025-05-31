5ac8d859f82a6b8ffcfbc54e7cb872ddd356ecd2
kqnade <git@k4na.de>
2025-05-31 15:37:07 +0900

    :zap: Update: new theme kanagawa

--- home/kqnade/modules/nixvim/plugins/themes.nix
+++ home/kqnade/modules/nixvim/plugins/themes.nix
@@ -1,5 +1,23 @@
 {
-  colorschemes.melange = {
-    enable = true;
+  colorschemes = {
+    melange = {
+      enable = false;
+    };
+    kanagawa = {
+      enable = true;
+      settings = {
+        commentStyle = {
+          italic = true;
+        };
+        compile = false;
+        dimInactive = false;
+        functionStyle = { };
+        overrides = "function(colors) return {} end";
+        terminalColors = true;
+        theme = "wave";
+        transparent = true;
+        undercurl = true;
+      };
+    };
   };
 }
