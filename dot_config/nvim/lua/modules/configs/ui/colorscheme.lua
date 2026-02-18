2df5587cfd7c135afd9cfa0bfdb275e06cfb8e7d
kqnade <git@k4na.de>
2026-02-18 16:03:55 +0900

    :sparkles: set trans for nvim

--- dot_config/nvim/lua/modules/configs/ui/colorscheme.lua
+++ dot_config/nvim/lua/modules/configs/ui/colorscheme.lua
@@ -38,3 +38,7 @@ require("onedarkpro").setup({
 
 -- Apply the colorscheme
 vim.cmd("colorscheme onedark")
+
+-- Inherit background from Kitty (no bg override, keeps terminal's color)
+vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE" })
