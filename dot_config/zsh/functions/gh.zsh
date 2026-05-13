8d2a7424c471b82f835fd996a299ee3baa642e0f
kqnade <git@k4na.de>
2026-05-13 15:51:38 +0900

    🔧 chore: update pass entry path for GitHub token

--- dot_config/zsh/functions/gh.zsh
+++ dot_config/zsh/functions/gh.zsh
@@ -1,8 +1,8 @@
 unalias gh 2>/dev/null
 gh() {
     if [[ -t 0 && -z "$GH_TOKEN" ]]; then
-        if pass github/token >/dev/null 2>&1; then
-            export GH_TOKEN=$(pass github/token)
+        if pass show token/github >/dev/null 2>&1; then
+            export GH_TOKEN=$(pass show token/github)
         fi
     fi
     command gh "$@"
